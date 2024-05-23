9ba7a12acddad15f90806dbecd8550e3d92b0f76
Victor Agbai <Ahfricanace@gmail.com>
2024-05-22 19:28:07 -0700

    0x0D. SQL - Introduction

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,1 +1,2 @@
+-- Deletes/Remove or Drop the database
 DROP DATABASE IF EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,4 @@
+-- Create a tables with a capacity to accept integers and variable characters
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
